890f24e32a97aba5c48da7ce431dff84797cc945
a-2280 <khaliff.williamss@gmail.com>
2024-09-16 15:03:34 -0700

    finished adding a report

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 with open('./books/frankenstein.txt') as frankenstein:
     contents = frankenstein.read()
 
-
 words = contents.split()
-print(len(words))
 
-character_count = {}
+character_count = { }
 for word in words:
     lowered_word = word.lower()
-    for letters in word:
-        print(letters)
+    for letters in lowered_word:
+        if letters in character_count:
+            character_count[letters] += 1
+        else:
+            character_count[letters] = 1
+
+print('--- Begin report of books/frankenstein.txt ---')
+print(f'{len(words)} words found in the document')
+print()
+sorted_items = sorted(character_count.items())
+for character, count in sorted_items:
+    print(f'The {character} character was found {count} times')
+print('--- End report ---')
